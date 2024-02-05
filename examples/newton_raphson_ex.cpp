7d40a86d8a246919a512a397f8d1ba297617e224
Priyanka <priyanka.bhovad@gmail.com>
2024-02-05 10:38:19 -0800

    updated example to vector function

--- examples/newton_raphson_ex.cpp
+++ examples/newton_raphson_ex.cpp
@@ -15,31 +15,32 @@ class FindRoot: public NewtonRaphson {
                 double epsilon, int max_iterations) : NewtonRaphson(num_states, num_equations, q0, 
                                             epsilon, max_iterations) {};
         VectorXd f(VectorXd q) override {
-            VectorXd v(1);
-            v << q(0)*q(0) - 1.0;
+            VectorXd v(2);
+            v << q(0)*q(0) - 1.0, q(1)*q(1) - 2.0;
             return v;};
         
         MatrixXd dfdq(VectorXd q) override {
-            MatrixXd mat(1, 1);
-            mat << 2*q(0);
+            MatrixXd mat(2, 2);
+            mat << 2*q(0), 0.0,
+                    0.0, 2*q(1);
             return mat;};
 
 };
 
 int main() {
-    int num_states = 1;
-    int num_equations = 1;
-    VectorXd q0(1);
-    q0 << 0.5;
-    double epsilon = 1e-3;
+    int num_states = 2;
+    int num_equations = 2;
+    VectorXd q0(num_states);
+    q0 << 0.5, 0.5;
+    double tolerance = 1e-3;
     int max_iterations = 50;
 
     NewtonRaphson* eq1 = new FindRoot(num_states, num_equations, q0, 
-                                            epsilon, max_iterations);
+                                            tolerance, max_iterations);
 
     eq1->Iterate();
     std::vector<VectorXd> q_history = eq1->GetSolutionHistory();
-    std::cout << "Solution, q = " << q_history.back() << "\n";
+    std::cout << "Solution, q =\n" << q_history.back() << "\n";
     delete eq1;
     return 0;
 }
