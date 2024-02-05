a1de2445744135ac37be33da142a4d0d7f607ba4
Priyanka <priyanka.bhovad@gmail.com>
2024-02-05 10:29:10 -0800

    created example for newton raphson method

--- examples/newton_raphson_ex.cpp
+++ examples/newton_raphson_ex.cpp
@@ -9,10 +9,37 @@
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
-class FindRoots: public NewtonRaphson {
+class FindRoot: public NewtonRaphson {
     public:
-        VectorXd NewtonRaphson::f(VectorXd q) override {return VectorXd v(num_equations)};
+        FindRoot(int num_states, int num_equations, VectorXd q0, 
+                double epsilon, int max_iterations) : NewtonRaphson(num_states, num_equations, q0, 
+                                            epsilon, max_iterations) {};
+        VectorXd f(VectorXd q) override {
+            VectorXd v(1);
+            v << q(0)*q(0) - 1.0;
+            return v;};
         
-        MatrixXd NewtonRaphson::dfdq(VectorXd q) {return MatrixXd mat(num_equations, num_states)};
+        MatrixXd dfdq(VectorXd q) override {
+            MatrixXd mat(1, 1);
+            mat << 2*q(0);
+            return mat;};
 
 };
+
+int main() {
+    int num_states = 1;
+    int num_equations = 1;
+    VectorXd q0(1);
+    q0 << 0.5;
+    double epsilon = 1e-3;
+    int max_iterations = 50;
+
+    NewtonRaphson* eq1 = new FindRoot(num_states, num_equations, q0, 
+                                            epsilon, max_iterations);
+
+    eq1->Iterate();
+    std::vector<VectorXd> q_history = eq1->GetSolutionHistory();
+    std::cout << q_history[0] << "\n";
+    delete eq1;
+    return 0;
+}
