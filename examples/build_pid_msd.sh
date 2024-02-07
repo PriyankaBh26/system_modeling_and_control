e88b6541baa08ac85150ecd380df9a6efa9d70d4
Priyanka <priyanka.bhovad@gmail.com>
2024-02-07 14:33:38 -0800

    updated system model to external class

--- examples/build_pid_msd.sh
+++ examples/build_pid_msd.sh
@@ -22,6 +22,7 @@ LOCAL_INCLUDE_FLAGS="-I$LOCAL_INCLUDE_PATH"
 SRCS="examples/pid_msd.cpp 
       src/controllers/pid_controller.cpp 
       src/numerical_solvers/rk_ode_solver.cpp 
+      src/system_models/mass_spring_damper.cpp
       src/numerical_solvers/solver_helper_funs.cpp 
       src/data_logging/savecsv.cpp
       src/data_logging/data_logging_helper_funs.cpp"
