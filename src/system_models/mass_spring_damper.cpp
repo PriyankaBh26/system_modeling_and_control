95a84b4f286e3d486e485d2570337fb350ca1b8d
Priyanka <priyanka.bhovad@gmail.com>
2024-02-07 09:44:11 -0800

    created mass spring damper class

---
+++ src/system_models/mass_spring_damper.cpp
@@ -0,0 +1,36 @@
+# include <vector>
+# include <Eigen/Dense>
+
+# include "numerical_solvers/rk_ode_solver.h"
+# include "system_models/mass_spring_damper.h"
+
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+
+MassSpringDamperSys::MassSpringDamperSys(VectorXd y0, double t0, 
+                                        double dt0, int num_states, std::string name,
+                                        double k0, double c0, double m0) : OdeSolver(y0, t0, dt0), 
+                                                                        A(num_states, num_states), 
+                                                                        B(num_states), 
+                                                                        sys_name(name),
+                                                                        k(k0), c(c0), m(m0) {
+                                                                        A << 0, 1,
+                                                                            -k/m, -c/m;
+                                                                        B << 0, 1;
+                                                                        };
+
+VectorXd MassSpringDamperSys::f(double t, VectorXd y, VectorXd u) {
+    VectorXd yd = A * y + B * u;
+    return yd;
+};                                                              
+
+std::string MassSpringDamperSys::GetName() {
+    return sys_name;
+}
+
+std::vector<std::string> MassSpringDamperSys::GetColumnNames() {
+    return {"Pos", "Vel"};
+}
+
+MatrixXd MassSpringDamperSys::GetA() {return A;};
+VectorXd MassSpringDamperSys::GetB() {return B;};
