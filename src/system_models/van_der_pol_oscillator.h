7e2c0acf6eab195526722806aef3022d9f73a9d4
Priyanka <priyanka.bhovad@gmail.com>
2024-02-07 10:31:49 -0800

    created van der pol oscillator class

---
+++ src/system_models/van_der_pol_oscillator.h
@@ -0,0 +1,34 @@
+#ifndef VAN_DER_POL_OSCILLATOR_H
+#define VAN_DER_POL_OSCILLATOR_H
+
+# include <Eigen/Dense>
+# include "numerical_solvers/rk_ode_solver.h"
+
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+
+class VanDerPolOscillator : public OdeSolver {
+    public:
+        VanDerPolOscillator(VectorXd x0, double t0, double dt0, 
+                            int num_states, std::string name, double mu0);
+
+        VectorXd f(double t, VectorXd X, VectorXd u) override;
+
+        MatrixXd CalculateFxJacobian(VectorXd X);
+
+        std::string GetName() override;
+
+        std::vector<std::string> GetColumnNames() override;
+        
+        VectorXd GetB();
+
+    private:
+        int num_states;
+        MatrixXd A;
+        VectorXd B;
+        std::string sys_name;
+        double mu;
+
+};
+
+#endif
