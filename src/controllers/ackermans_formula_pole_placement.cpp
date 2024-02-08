a616e416427af0fb99f81f2c9491a015ed9dd3c4
Priyanka <priyanka.bhovad@gmail.com>
2024-02-08 12:50:40 -0800

    created ackermans formula pole placement class

---
+++ src/controllers/ackermans_formula_pole_placement.cpp
@@ -0,0 +1,102 @@
+# include <vector>
+# include <string>
+# include <cmath>
+# include <Eigen/Dense>
+
+# include "controllers/ackermans_formula_pole_placement.h"
+
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+using Eigen::VectorXcd;
+
+AckermansFormulaPolePlacement::AckermansFormulaPolePlacement(std::string str, 
+                                                            VectorXd ip_vec, 
+                                                            MatrixXd A_in,
+                                                            MatrixXd B_in) : ip_vector_type(str), 
+                                                                            A(A_in),
+                                                                            B(B_in) {
+    n = A.rows();
+    VectorXd K(n,1);
+    if (ip_vector_type == "roots") {
+        desired_roots = ip_vec;
+    } else if (ip_vector_type == "coeffs") {
+        polynomial_coefficients = ip_vec;
+    }
+    
+};
+
+
+MatrixXd AckermansFormulaPolePlacement::CharacteristicPolynomial() {
+    MatrixXd delta_A = MatrixXd::Identity(n, n);
+
+    if (ip_vector_type == "roots") {
+        for (const auto& root : desired_roots) {
+            delta_A = delta_A * (A - root * MatrixXd::Identity(n, n));
+        }
+    } else if (ip_vector_type == "coeffs") {
+        MatrixXd A_pow = MatrixXd::Identity(n, n);
+        delta_A = delta_A * polynomial_coefficients(n);
+        for (int i{1}; i<=n; i++) {
+            A_pow = A_pow * A;
+            delta_A = delta_A + A_pow * polynomial_coefficients(n - i);
+        }
+    }
+    return delta_A;
+};
+
+MatrixXd AckermansFormulaPolePlacement::ControllabilityMatrix() {
+    MatrixXd C(n, n);
+    MatrixXd A_pow = MatrixXd::Identity(n, n);
+    for (int i{0}; i<n; i++) {
+        C.block(0,i,n,1) = A_pow * B;
+        A_pow = A_pow * A;
+    }
+    return C;
+};
+
+VectorXd AckermansFormulaPolePlacement::AckermansFormula() {
+    VectorXd one_n(n);
+    one_n(n-1) = 1;
+    std::cout << "\none_n:\n" << one_n;
+    MatrixXd C = AckermansFormulaPolePlacement::ControllabilityMatrix();
+    MatrixXd delta_A = AckermansFormulaPolePlacement::CharacteristicPolynomial();
+    // K = [0 0 .. 1] * C.inv() * delta(A);
+    K = one_n.transpose() * C.inverse() * delta_A;
+    return K.transpose();
+
+};
+
+MatrixXd AckermansFormulaPolePlacement::CalculateResultantA() {
+    MatrixXd resultant_A(n,n);
+    resultant_A = A - B * K.transpose();
+    return resultant_A;
+}
+
+VectorXcd AckermansFormulaPolePlacement::CalculateEigenValues(MatrixXd ip_mat) {
+    // Compute the eigenvalues
+    Eigen::EigenSolver<MatrixXd> solver(ip_mat);
+    VectorXcd eigenvalues = solver.eigenvalues();
+    return eigenvalues;
+}
+
+std::ostream& operator << (std::ostream& out, AckermansFormulaPolePlacement& system) {
+
+    out << "\nPrinting Feedback gains for given system using Ackerman's formula:\n";
+
+    if (system.ip_vector_type == "roots") {
+        out << "desired roots: \n";
+        out << system.desired_roots << "\n";
+
+    } else if (system.ip_vector_type == "coeffs") {
+        out << "desired coefficients: \n";
+        out << system.polynomial_coefficients << "\n";
+    }
+        out << "(A) State matrix: \n";
+        out << system.A << "\n";
+        out << "(B) Input Vector: \n";
+        out << system.B << "\n";
+        out << "(K) Feedback gain vector: \n";
+        out << system.K << "\n";
+    return out;
+};
+
