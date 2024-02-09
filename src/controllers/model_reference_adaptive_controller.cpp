ff9ee3344ae096fef72d849ffd876696fe28876b
Priyanka <priyanka.bhovad@gmail.com>
2024-02-09 09:05:08 -0800

    created MRAC class

---
+++ src/controllers/model_reference_adaptive_controller.cpp
@@ -0,0 +1,149 @@
+# include <string>
+# include <iostream>
+# include <random>
+# include <cmath>
+
+# include <Eigen/Dense>
+
+# include "controllers/model_reference_adaptive_controller.h"
+
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+
+
+DirectMRAC::DirectMRAC(
+    std::string str,
+    int features,
+    VectorXd d0,
+    int n,
+    double dt0,
+    VectorXd w0,
+    double gamma_w,
+    double gamma_v,
+    double gamma_kx,
+    double gamma_kr
+    MatrixXd A_nom,
+    MatrixXd B_in) :   disturbance_model_feature_type(str),
+                        num_features(features),
+                        disturbance_mean_std_bw(d0),
+                        num_states(n),
+                        dt(dt0),
+                        w(w0), error(n), 
+                        V(features),
+                        Kx(n), Kr(n),
+                        learning_rate_w(gamma_w),
+                        learning_rate_v(gamma_v),
+                        learning_rate_kx(gamma_kx),
+                        learning_rate_kr(gamma_kr)
+                        A_nominal(A_nom),
+                        B(B_in) {
+    CalculateMatrixP();
+    // Define a random number generator
+    std::default_random_engine generator;
+
+    std::normal_distribution<double> centers_distribution(disturbance_mean_std_bw(0), disturbance_mean_std_bw(1));
+    std::normal_distribution<double> V_distribution(0, disturbance_mean_std_bw(2));
+
+    VectorXd centers(num_features);
+    VectorXd V(num_features);
+    for (int i{0}; i<num_features; i++) {
+        centers(i) = centers_distribution(generator);
+        V(i) = abs(V_distribution(generator));
+    }
+};
+
+VectorXd DirectMRAC::phi(VectorXd x) {
+    // radial basis function
+    VectorXd phi_x(num_states);
+    int i = 0;
+    for (int i{0}; i<num_states; i++) {    
+        VectorXd phi_x_f = (centers - x(i)) / V;
+        phi_x_f = - phi_x_f.square();
+        phi_x_f = phi_x_f.exp();
+        phi_x(i) = sum(phi_x_f);
+    }
+return phi_x;
+};
+
+VectorXd DirectMRAC::sigmoid(VectorXd Y) {
+    // single hidden layer Neural Network with sigmoid activation fun
+    VectorXd s_x(num_features);
+    for (int i{0}; i<num_features; i++) {
+        s_x(i) = 1/(1 + exp(-Y(i)));
+    }
+    return s_x;
+};
+        
+VectorXd DirectMRAC::dsigmoid(VectorXd Y) {
+    // derivative of sigmoid activation fun
+    VectorXd ds_x(num_features);
+    for (int i{0}; i<num_features; i++) {
+        double s_x = 1/(1 + exp(-Y(i)));
+        ds_x(i) = s_x * (1 - s_x);
+    }
+    return ds_x;
+};
+
+DirectMRAC::UpdateWeights(VectorXd x) {
+    if (disturbance_model_feature_type = "radial_basis_fun") {
+        wdot = learning_rate_w * phi(x) * error.transpose() * P * B;
+        w = w + wdot * dt;
+    } else if (disturbance_model_feature_type = "single_hidden_layer_nn") {
+        wdot = -(sigmoid(V.transpose() * x) - dsigmoid(V.transpose() * x) * V.transpose() * x) * error.transpose() * P * B * learning_rate_w;
+        Vdot = -learning_rate_v * x * error.transpose() * P * B * w.transpose() * sigmoid(V.transpose() * x);
+        w = w + wdot * dt;
+        V = V + Vdot * dt;
+    }
+};
+
+DirectMRAC::UpdateKx(VectorXd x) {
+    Kxdot = learning_rate_Kx * x * error.transpose() * P * B;
+    Kx = Kx + Kxdot * dt;
+};
+
+DirectMRAC::UpdateKr(VectorXd r) {
+    Krdot = learning_rate_Kr * r * error.transpose() * P * B;
+    Kr = Kr + Krdot * dt;
+};
+
+DirectMRAC::CalculateMatrixP() {
+    // Solve the Lyapunov equation AmP + PAm^T = -Q
+    MatrixXd Q = MatrixXd::Identity(num_states, num_states);
+    MatrixXd P = MatrixXd::Zero(num_states, num_states);
+    LLT<MatrixXd> lltOfA(A_nominal.transpose());
+    if (lltOfA.info() == Success) {
+        // A is positive definite
+        P = lltOfA.solve(-Q);
+        P = (P + P.transpose()) / 2; // Ensure symmetric solution
+    } else {
+        // A is not positive definite
+        std::cout << "Matrix A is not positive definite. Cannot solve Lyapunov equation." << std::endl;
+    }
+
+};
+
+DirectMRAC::CalculateError(VectorXd x_ref, VectorXd x) {
+    error = x - x_ref;
+};
+
+VectorXd DirectMRAC::UpdateAdaptiveControlInput(VectorXd x) {
+    UpdateWeights(VectorXd x);
+    VectorXd u_ad(1);
+    if (disturbance_model_feature_type = "radial_basis_fun") {
+        u_ad = w.transpose() * phi(x);
+    } else if (disturbance_model_feature_type = "single_hidden_layer_nn") {
+        u_ad = w.transpose() * sigmoid(V.transpose() * x)
+    return u_ad;
+};
+
+VectorXd DirectMRAC::UpdateControlInput(VectorXd r, VectorXd x_ref, VectorXd x) {
+    DirectMRAC::CalculateError(VectorXd x_ref, VectorXd x);
+
+    UpdateKx(VectorXd x);
+    UpdateKr(VectorXd r);
+
+    VectorXd u_ad = DirectMRAC::UpdateAdaptiveControlInput(VectorXd x);
+    VectorXd u = - Kx * x + Kr * r - u_ad;
+};
+
+friend std::ostream& operator << (std::ostream& out, DirectMRAC& system);
--- src/controllers/model_reference_adaptive_controller.h
+++ src/controllers/model_reference_adaptive_controller.h
@@ -1,5 +1,66 @@
 #ifndef MODEL_REFERENCE_ADAPTIVE_CONTROLLER_H
 #define MODEL_REFERENCE_ADAPTIVE_CONTROLLER_H
 
+# include <string>
+# include <iostream>
+
+# include <Eigen/Dense>
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+
+class DirectMRAC {
+    public:
+        DirectMRAC(std::string disturbance_model_feature_type,
+                    int num_features,
+                    VectorXd disturbance_mean_std,
+                    int num_states,
+                    double dt,
+                    VectorXd w0,
+                    double learning_rate_w,
+                    double learning_rate_v,
+                    double learning_rate_kx,
+                    double learning_rate_kr
+                    MatrixXd A_nominal,
+                    MatrixXd B);
+
+        VectorXd phi(VectorXd x);
+
+        VectorXd sigmoid(VectorXd Y);
+        
+        VectorXd dsigmoid(VectorXd Y);
+
+        UpdateWeights(VectorXd x);
+
+        UpdateKx(VectorXd x);
+
+        UpdateKr(VectorXd r);
+
+        CalculateError(VectorXd x_ref, VectorXd x);
+
+        CalculateMatrixP();
+
+        VectorXd UpdateAdaptiveControlInput(VectorXd x);
+
+        VectorXd UpdateControlInput(VectorXd r, VectorXd x_ref, VectorXd x);
+
+        friend std::ostream& operator << (std::ostream& out, DirectMRAC& system);
+
+    private:
+        std::string disturbance_model_feature_type;
+        int num_features;
+        VectorXd disturbance_mean_std_bw;
+        VectorXd centers;
+        VectorXd V;
+        int num_states;
+        double dt;
+        VectorXd w;
+        double learning_rate_w;
+        double learning_rate_v;
+        double learning_rate_kx;
+        double learning_rate_kr;
+        MatrixXd A_nominal;
+        MatrixXd B;
+        VectorXd error;
+};
 
 #endif
