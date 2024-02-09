2802dd72098a6ddaacb666f54258afd33f6ad0a4
Priyanka <priyanka.bhovad@gmail.com>
2024-02-09 11:57:12 -0800

    added eigen namespaces

--- src/numerical_solvers/rk_ode_solver.h
+++ src/numerical_solvers/rk_ode_solver.h
@@ -3,35 +3,38 @@
 
 # include <Eigen/Dense>
 
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+
 class OdeSolver {
     public:
-        OdeSolver(Eigen::VectorXd x0, double t0, double dt0);
+        OdeSolver(VectorXd x0, double t0, double dt0);
         virtual ~OdeSolver();
 
-        virtual Eigen::VectorXd f(double t, Eigen::VectorXd X, Eigen::VectorXd u);
+        virtual VectorXd f(double t, VectorXd X, VectorXd u);
         virtual MatrixXd CalculateFxJacobian(VectorXd X);
 
-        virtual Eigen::VectorXd h(double t, Eigen::VectorXd X, Eigen::VectorXd u);
+        virtual VectorXd h(double t, VectorXd X, VectorXd u);
         virtual MatrixXd CalculateHxJacobian(VectorXd X);
 
         virtual std::string GetName();
         virtual std::vector<std::string> GetColumnNames();
         
-        std::vector<std::string> GetControlInputColumnNames(Eigen::VectorXd u);
+        std::vector<std::string> GetControlInputColumnNames(VectorXd u);
 
-        void RK4Update(Eigen::VectorXd u);
-        void IntegrateODE(int timesteps, Eigen::VectorXd u);
+        void RK4Update(VectorXd u);
+        void IntegrateODE(int timesteps, VectorXd u);
 
-        Eigen::VectorXd GetX();
+        VectorXd GetX();
         double GetT();
-        std::vector<Eigen::VectorXd> GetXHistory();
+        std::vector<VectorXd> GetXHistory();
         std::vector<double> GetTHistory();
 
     private:
-        Eigen::VectorXd x;
+        VectorXd x;
         double t;
         double dt;
-        std::vector<Eigen::VectorXd> x_history;
+        std::vector<VectorXd> x_history;
         std::vector<double> t_history;
 
 };
