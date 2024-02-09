f0f9bc8191fd2cf0137e086223a0bb9f24e85a99
Priyanka <priyanka.bhovad@gmail.com>
2024-02-09 11:26:45 -0800

    added comments and re-organized parameters

--- test/controllers/test_MRAC.cpp
+++ test/controllers/test_MRAC.cpp
@@ -10,6 +10,32 @@ using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
 int main() {
+
+    // reference model matrices A_ref and B_ref
+    MatrixXd A_ref(num_states, num_states);
+    A_ref << 0 , 1,
+            -4, -2;
+    // Compute the eigenvalues of A_ref
+    Eigen::EigenSolver<MatrixXd> solver(A_ref);
+    Eigen::VectorXcd eigenvalues = solver.eigenvalues();
+    std::cout << "A_ref eigenvalues:\n"  << eigenvalues;
+
+    MatrixXd B_ref(1, num_states);
+    B_ref << 0, 4;
+
+    // nominal plant model matrices A and B
+    MatrixXd A(num_states, num_states);
+    A_ << 0, 1,
+          0, 0;
+    MatrixXd B(1, num_states);
+    B << 0, 1;
+
+    // solved Lyapunov equation using controller helper funs and A_ref
+    MatrixXd P(num_states, num_states);
+    P << 1.5, 0.125,
+         0.125, 0.3125;
+
+    // initialize MRAC model
     std::string disturbance_model_feature_type = "radial_basis_fun";
     int num_features = 10;
     VectorXd disturbance_mean_std_bw(3);
@@ -20,20 +46,7 @@ int main() {
     double learning_rate_v = 10;
     double learning_rate_kx = 10;
     double learning_rate_kr = 10;
-    MatrixXd A_ref(num_states, num_states);
-    A_ref << 0 , 1,
-            -4, -2;
-    // Compute the eigenvalues of A_ref
-    Eigen::EigenSolver<MatrixXd> solver(A_ref);
-    Eigen::VectorXcd eigenvalues = solver.eigenvalues();
-    std::cout << "A_ref eigenvalues:\n"  << eigenvalues;
-
-    MatrixXd B(1, num_states);
-    B << 0, 1;
-
-    MatrixXd P(num_states, num_states);
-    P << 1.5, 0.125,
-         0.125, 0.3125;
+    
     DirectMRAC* mrac = new DirectMRAC(disturbance_model_feature_type,
                                       num_features,
                                       disturbance_mean_std_bw,
