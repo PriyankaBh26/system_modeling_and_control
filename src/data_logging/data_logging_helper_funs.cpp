c5b884a1147c36b3a44d3b26c74bc5ff3bc956ae
Priyanka <priyanka.bhovad@gmail.com>
2024-02-10 16:17:54 -0800

    removed unused data logging functions

--- src/data_logging/data_logging_helper_funs.cpp
+++ src/data_logging/data_logging_helper_funs.cpp
@@ -4,122 +4,10 @@
 # include <Eigen/Dense>
 
 # include "data_logging/data_logging_helper_funs.h"
-# include "numerical_solvers/rk_ode_solver.h"
-# include "controllers/pid_controller.h"
 # include "data_logging/savecsv.h"
 
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
-
-void SaveSimulationData(OdeSolver* ode, 
-                        PID* pid_controller, 
-                        std::vector<VectorXd>& x_history, 
-                        std::vector<double> t_history) {
-
-    // save final output x to csv file
-    std::string filename = "examples/" + ode->GetName() + "_solution.csv";
-    std::vector<std::string> columnNames = ode->GetColumnNames();
-    WriteMatToFile(filename, columnNames, x_history);
-    std::cout << x_history.size() << " " << x_history[0].size();
-
-    // save final error history to csv file
-    std::vector<VectorXd> error_history = pid_controller->GetErrorHistory();
-    filename = "examples/" + ode->GetName() + "_err_history.csv";
-    WriteMatToFile(filename, columnNames, error_history);
-
-    // save control input history to csv file
-    std::vector<VectorXd> control_input_history = pid_controller->GetControlInputHistory();
-    filename = "examples/" + ode->GetName() + "_control_history.csv";
-    columnNames = pid_controller->GetColumnNames();
-    WriteMatToFile(filename, columnNames, control_input_history);
-
-    // save time to csv file
-    filename = "examples/" + ode->GetName() + "_time.csv";
-    columnNames = {"time"};
-    WriteVecToFile(filename, columnNames, t_history);
-}
-
-
-void SaveSimulationData(OdeSolver* ode, 
-                        std::vector<VectorXd>& x_history, 
-                        std::vector<double> t_history, 
-                        std::vector<VectorXd>& u_history) {
-
-    // save final output x to csv file
-    std::string filename = "examples/" + ode->GetName() + "_solution.csv";
-    std::vector<std::string> columnNames = ode->GetColumnNames();
-    WriteMatToFile(filename, columnNames, x_history);
-    std::cout << x_history.size() << " " << x_history[0].size();
-
-    // save control input history to csv file
-    filename = "examples/" + ode->GetName() + "_control_history.csv";
-    columnNames = ode->GetControlInputColumnNames(u_history[0]);
-    WriteMatToFile(filename, columnNames, u_history);
-
-    // save time to csv file
-    filename = "examples/" + ode->GetName() + "_time.csv";
-    columnNames = {"time"};
-    WriteVecToFile(filename, columnNames, t_history);
-}
-
-void SaveKFSimulationData(OdeSolver* ode, 
-                          std::vector<VectorXd>& x_history, 
-                          std::vector<double> t_history, 
-                          std::vector<VectorXd>& x_est_history, 
-                          std::vector<VectorXd>& z_history) {
-
-    // save final output x to csv file
-    std::string filename = "examples/" + ode->GetName() + "_solution.csv";
-    std::vector<std::string> column_names = ode->GetColumnNames();
-    WriteMatToFile(filename, column_names, x_history);
-    std::cout << x_history.size() << " " << x_history[0].size();
-
-    // save estimated state history to csv file
-    filename = "examples/" + ode->GetName() + "_meas_history.csv";
-    WriteMatToFile(filename, column_names, z_history);
-
-    // save estimated state history to csv file
-    filename = "examples/" + ode->GetName() + "_est_history.csv";
-    WriteMatToFile(filename, column_names, x_est_history);
-
-    // save time to csv file
-    filename = "examples/" + ode->GetName() + "_time.csv";
-    column_names = {"time"};
-    WriteVecToFile(filename, column_names, t_history);
-}
-
-
-void SaveKFPIDSimulationData(OdeSolver* ode, 
-                        std::vector<VectorXd>& x_history, 
-                        std::vector<double> t_history, 
-                        std::vector<VectorXd>& x_est_history, 
-                        std::vector<VectorXd>& z_history,
-                        std::vector<VectorXd>& u_history) {
-
-    // save final output x to csv file
-    std::string filename = "examples/" + ode->GetName() + "_solution.csv";
-    std::vector<std::string> column_names = ode->GetColumnNames();
-    WriteMatToFile(filename, column_names, x_history);
-    std::cout << x_history.size() << " " << x_history[0].size();
-
-    // save control history to csv file
-    filename = "examples/" + ode->GetName() + "_control_history.csv";
-    std::vector<std::string> u_column_names = {"U1", "U2"};
-    WriteMatToFile(filename, u_column_names, u_history);
-
-    // save measured state history to csv file
-    filename = "examples/" + ode->GetName() + "_meas_history.csv";
-    WriteMatToFile(filename, column_names, z_history);
-
-    // save estimated state history to csv file
-    filename = "examples/" + ode->GetName() + "_est_history.csv";
-    WriteMatToFile(filename, column_names, x_est_history);
-
-    // save time to csv file
-    filename = "examples/" + ode->GetName() + "_time.csv";
-    column_names = {"time"};
-    WriteVecToFile(filename, column_names, t_history);
-}
 
 void SaveSimDataHistory(std::string directory, 
                         std::string problem, 
@@ -130,7 +18,7 @@ void SaveSimDataHistory(std::string directory,
     // save final output x to csv file
     std::string filename = directory + "/" + problem + "_" + data_type + ".csv";
     WriteMatToFile(filename, column_names, x_history);
-    std::cout << x_history.size() << " " << x_history.size();
+    std::cout << "\n" + data_type + " size: " << x_history.size() << " " << x_history.size();
 }
 
 
@@ -141,5 +29,5 @@ void SaveTimeHistory(std::string directory,
     // save final output x to csv file
     std::string filename = directory + "/" + problem + "_time.csv";
     WriteVecToFile(filename, {"time"}, t_history);
-    std::cout << "time history size: " << t_history.size();
+    std::cout << "\ntime history size: " << t_history.size();
 }
--- src/data_logging/data_logging_helper_funs.h
+++ src/data_logging/data_logging_helper_funs.h
@@ -2,34 +2,9 @@
 #define DATA_LOGGING_HELPER_FUNS_H
 
 # include <Eigen/Dense>
-# include "numerical_solvers/rk_ode_solver.h"
-# include "controllers/pid_controller.h"
 
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
-
-void SaveSimulationData(OdeSolver* ode, 
-                        PID* pid_controller, 
-                        std::vector<VectorXd>& x_history, 
-                        std::vector<double> t_history);
-
-void SaveSimulationData(OdeSolver* ode, 
-                        std::vector<VectorXd>& x_history, 
-                        std::vector<double> t_history, 
-                        std::vector<VectorXd>& u_history);
-
-void SaveKFSimulationData(OdeSolver* ode, 
-                          std::vector<VectorXd>& x_history, 
-                          std::vector<double> t_history, 
-                          std::vector<VectorXd>& x_est_history, 
-                          std::vector<VectorXd>& z_history);
-
-void SaveKFPIDSimulationData(OdeSolver* ode, 
-                        std::vector<VectorXd>& x_history, 
-                        std::vector<double> t_history, 
-                        std::vector<VectorXd>& x_est_history, 
-                        std::vector<VectorXd>& z_history,
-                        std::vector<VectorXd>& u_history);
 
 void SaveSimDataHistory(std::string directory, 
                         std::string problem, 
