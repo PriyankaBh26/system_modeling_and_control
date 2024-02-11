b4b34d5e0d1f591323e094aea3e3cc07f4688c41
Priyanka <priyanka.bhovad@gmail.com>
2024-02-11 13:48:10 -0800

    updated structure names

--- examples/nlopt_usage.cpp
+++ examples/nlopt_usage.cpp
@@ -8,11 +8,11 @@
 
 typedef struct {
     int count;
-} counter;
+} obj_arguments;
 
 double objective(const std::vector<double> &x, std::vector<double> &grad, void *data)
 {   
-    counter *d = reinterpret_cast<counter*>(data);
+    obj_arguments *d = reinterpret_cast<obj_arguments*>(data);
     ++d->count;
     if (!grad.empty()) {
         grad[0] = 0.0;
@@ -23,11 +23,11 @@ double objective(const std::vector<double> &x, std::vector<double> &grad, void *
 
 typedef struct {
     double a, b;
-} my_constraint_data;
+} constraint_arguments;
 
 double myconstraint(const std::vector<double> &x, std::vector<double> &grad, void *data)
 {
-    my_constraint_data *d = reinterpret_cast<my_constraint_data*>(data);
+    constraint_arguments *d = reinterpret_cast<constraint_arguments*>(data);
     double a = d->a, b = d->b;
     if (!grad.empty()) {
         grad[0] = 3 * a * std::pow((a*x[0] + b), 2);
@@ -43,12 +43,12 @@ int main() {
     std::vector<double> lb = {-HUGE_VAL, 0};
     opt.set_lower_bounds(lb);
 
-    counter counter_data = {0};
-    opt.set_min_objective(objective, &counter_data);
+    obj_arguments obj_args = {0};
+    opt.set_min_objective(objective, &obj_args);
 
-    my_constraint_data data[2] = { {2,0}, {-1,1} };
-    opt.add_inequality_constraint(myconstraint, &data[0], 1e-8);
-    opt.add_inequality_constraint(myconstraint, &data[1], 1e-8);
+    constraint_arguments constraint_args[2] = { {2,0}, {-1,1} };
+    opt.add_inequality_constraint(myconstraint, &constraint_args[0], 1e-8);
+    opt.add_inequality_constraint(myconstraint, &constraint_args[1], 1e-8);
 
     opt.set_xtol_rel(1e-10);
     std::vector<double> x = {1.234, 5.678};
@@ -56,7 +56,7 @@ int main() {
 
     try {
         nlopt::result result = opt.optimize(x, minf);
-        std::cout << "found minimum after " << counter_data.count << " evaluations\n";
+        std::cout << "found minimum after " << obj_args.count << " evaluations\n";
         std::cout << "result: " << result << "\n";
         std::cout << "found minimum at f(" << x[0] << "," << x[1] << ") = "
             << std::setprecision(10) << minf << std::endl;
