54c7965709bae7d3ceca5c9c0b00f2e2444759f6
Priyanka <priyanka.bhovad@gmail.com>
2024-02-11 21:51:25 -0800

    updated class declaration

--- src/multi_joint_robots/forward_kinematics.h
+++ src/multi_joint_robots/forward_kinematics.h
@@ -8,21 +8,21 @@ using Eigen::VectorXd;
 class ForwardKinematics {
     public:
         ForwardKinematics(int n, VectorXd L, std::vector<std::string> joint_type, 
-                          MatrixXd tf_0_se, MatrixXd screw_axes_s, MatrixXd screw_axes_e);
+                          MatrixXd tf_home, MatrixXd screw_axes_s, MatrixXd screw_axes_e);
 
-        void TfMatrixInBaseFrame();
+        void TfMatrixInBaseFrame(VectorXd q);
 
-        void TfMatrixInEEFrame();
+        void TfMatrixInEEFrame(VectorXd q);
 
-        MatrixXd KthJointMatrixInBaseFrame(int k);
+        MatrixXd KthJointMatrixInBaseFrame(VectorXd q, int k);
 
-        MatrixXd KthJointMatrixInEEFrame(int k);
+        MatrixXd KthJointMatrixInEEFrame(VectorXd q, int k);
 
         MatrixXd ExponentialMatrix(VectorXd screw_axis, double q_i, std::string joint_type_i);
 
-        MatrixXd SpaceJacobian();
+        MatrixXd SpaceJacobian(VectorXd q);
 
-        MatrixXd BodyJacobian();
+        MatrixXd BodyJacobian(VectorXd q);
 
         MatrixXd CalculateAdjointOfTfMatrix(MatrixXd tf_mat);
 
@@ -30,14 +30,17 @@ class ForwardKinematics {
     
         VectorXd CalculateJointTorques(MatrixXd jacobian, VectorXd F);
 
+        MatrixXd GetTfse();
+
+        MatrixXd GetTfes();
+
     private:
         int num_joints;
         VectorXd link_length;
         std::vector<std::string> joint_type;
-        MatrixXd tf_0_se;
+        MatrixXd tf_home;
         MatrixXd screw_axes_s;
         MatrixXd screw_axes_e;
-        VectorXd q;
         MatrixXd tf_se;
         MatrixXd tf_es;
 };
