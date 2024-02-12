0faafd746487ae9eea75484ddc67be4b5178efbc
Priyanka <priyanka.bhovad@gmail.com>
2024-02-11 17:00:04 -0800

    added build shell script

---
+++ test/multi_joint_robots/build_test_forward_kinemetics.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Compiler
+CXX=g++
+
+# Compiler flags
+CXXFLAGS="-std=c++17 -Wall"
+
+# Path to Eigen library (change this to match your Eigen installation path)
+EIGEN_PATH="/Users/priyankabhovad/Downloads/cpp_libs/eigen"
+
+# Include flags for Eigen
+INCLUDE_FLAGS="-I$EIGEN_PATH"
+
+# Include local include path
+LOCAL_INCLUDE_PATH="/Users/priyankabhovad/Downloads/cpp_ex/system_modeling_and_control/src"
+
+# Include flags for local header files
+LOCAL_INCLUDE_FLAGS="-I$LOCAL_INCLUDE_PATH"
+
+SRCS="test/multi_joint_robots/test_forward_kinematics.cpp 
+     src/data_logging/savecsv.cpp
+     src/multi_joint_robots/forward_kinematics.cpp"
+
+# Output executable name
+TARGET="build/myprogram"
+
+# Compilation command
+$CXX $CXXFLAGS $INCLUDE_FLAGS $LOCAL_INCLUDE_FLAGS -o $TARGET $SRCS
+
+# Check if compilation was successful
+if [ $? -eq 0 ]; then
+    echo "Compilation successful. Executable created: $TARGET"
+else
+    echo "Compilation failed."
+fi
