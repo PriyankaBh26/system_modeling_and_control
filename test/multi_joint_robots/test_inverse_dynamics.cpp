62aeb63bd0c213d9648b3861961bb3519bdcda5b
Priyanka <priyanka.bhovad@gmail.com>
2024-02-14 10:55:43 -0800

    added test for inverse dynamics class

---
+++ test/multi_joint_robots/test_inverse_dynamics.cpp
@@ -0,0 +1,12 @@
+# include <iostream>
+# include <Eigen/Dense>
+# include "multi_joint_robots/inverse_dynamics.h"
+
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+
+int main() {
+
+    
+    return 0;
+}
