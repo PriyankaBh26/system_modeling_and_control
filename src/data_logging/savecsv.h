e8c5801def9807f4821b866205e65dfeaf957f7e
Priyanka <priyanka.bhovad@gmail.com>
2024-02-16 14:13:52 -0800

    added option to append or replace the file contents

--- src/data_logging/savecsv.h
+++ src/data_logging/savecsv.h
@@ -7,8 +7,8 @@ void WriteColumnNames(std::vector<std::string> column_names, std::ofstream outpu
 
 void WriteToFile(std::vector<double> my_vec, std::ofstream output_file);
 
-void WriteMatToFile(std::string filename, std::vector<std::string> column_names, std::vector<Eigen::VectorXd> my_mat);
+void WriteMatToFile(std::string filename, std::vector<std::string> column_names, std::vector<Eigen::VectorXd> my_mat, std::string save_type);
 
-void WriteVecToFile(std::string filename, std::vector<std::string> column_names, std::vector<double> my_vec);
+void WriteVecToFile(std::string filename, std::vector<std::string> column_names, std::vector<double> my_vec, std::string save_type);
 
 #endif // SAVECSV_H
