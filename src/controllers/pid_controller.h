983f4242b301d447172b604dcfa74e73a7ac79cf
Priyanka <priyanka.bhovad@gmail.com>
2024-02-17 15:18:12 -0800

    added Eigen namespace

--- src/controllers/pid_controller.h
+++ src/controllers/pid_controller.h
@@ -3,34 +3,37 @@
 
 # include <Eigen/Dense>
 
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+
 class PID {
     public:
         PID(int NUM_STATES);
 
-        void SetGains(const Eigen::MatrixXd& KP, const Eigen::MatrixXd& KI, const Eigen::MatrixXd& KD);
+        void SetGains(const MatrixXd& KP, const MatrixXd& KI, const MatrixXd& KD);
 
-        void CalculateError(Eigen::VectorXd x_ref, Eigen::VectorXd x);
+        void CalculateError(VectorXd x_ref, VectorXd x);
 
-        std::vector<Eigen::VectorXd> GetErrorHistory();
+        std::vector<VectorXd> GetErrorHistory();
 
-        std::vector<Eigen::VectorXd> GetControlInputHistory();
+        std::vector<VectorXd> GetControlInputHistory();
 
-        Eigen::VectorXd GenerateControlInput();
+        VectorXd GenerateControlInput();
 
         std::vector<std::string> GetColumnNames();
 
         friend std::ostream& operator<<(std::ostream& out, const PID& PID);
 
     private:
-        Eigen::MatrixXd kp;
-        Eigen::MatrixXd ki;
-        Eigen::MatrixXd kd;
-        Eigen::VectorXd error;
-        Eigen::VectorXd d_error;
-        Eigen::VectorXd sum;
-        Eigen::VectorXd previous_error;
-        std::vector<Eigen::VectorXd> error_history;
-        std::vector<Eigen::VectorXd> control_input_history;
+        MatrixXd kp;
+        MatrixXd ki;
+        MatrixXd kd;
+        VectorXd error;
+        VectorXd d_error;
+        VectorXd sum;
+        VectorXd previous_error;
+        std::vector<VectorXd> error_history;
+        std::vector<VectorXd> control_input_history;
 
 };
 
