0bcee6b15a038b4446721e84de176447f70b82b3
Priyanka <priyanka.bhovad@gmail.com>
2024-02-17 16:10:38 -0800

    updated pid controller

--- src/controllers/pid_controller.cpp
+++ src/controllers/pid_controller.cpp
@@ -7,9 +7,11 @@
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
-PID::PID(int n) : kp(n,n), ki(n,n), kd(n,n), error(n), d_error(n), sum(n), previous_error(n) {
-    error_history.push_back(previous_error);
-    control_input_history.push_back(previous_error);
+PID::PID(int n) : num_states(n), kp(n,n), ki(n,n), kd(n,n) {
+    integral_err_history.push_back(VectorXd::Zero(n));
+    x_err_history.push_back(VectorXd::Zero(n));
+    dxdt_err_history.push_back(VectorXd::Zero(n));    
+    control_input_history.push_back(VectorXd::Zero(n));
     };
 
 void PID::SetGains(const MatrixXd& KP, const MatrixXd& KI, const MatrixXd& KD) {
@@ -18,34 +20,37 @@ void PID::SetGains(const MatrixXd& KP, const MatrixXd& KI, const MatrixXd& KD) {
     kd = KD;
 }
 
-void PID::CalculateError(VectorXd x_ref, VectorXd x) {
-    error = x_ref - x;
-    d_error = error - previous_error;
-    sum += error; 
-    previous_error = error;
-    // save error history
-    error_history.push_back(error);
+std::vector<VectorXd> PID::GetXErrorHistory() {
+    return x_err_history;
 }
 
-std::vector<VectorXd> PID::GetErrorHistory() {
-    return error_history;
+std::vector<VectorXd> PID::GetDxdtErrorHistory() {
+    return dxdt_err_history;
+}
+
+std::vector<VectorXd> PID::GetIntegralErrorHistory() {
+    return integral_err_history;
 }
 
 std::vector<VectorXd> PID::GetControlInputHistory() {
     return control_input_history;
 }
 
-VectorXd PID::GenerateControlInput() {
-    VectorXd u = kp * error + ki * sum + kd * d_error;
+VectorXd PID::GenerateControlInput(VectorXd x_err, VectorXd dxdt_err) {
+    VectorXd sum = integral_err_history.back() + x_err;
+    VectorXd u = kp * x_err + ki * sum + kd * dxdt_err;
     // save control input history
     control_input_history.push_back(u);
+    // save error history
+    x_err_history.push_back(x_err);
+    dxdt_err_history.push_back(dxdt_err);
+    integral_err_history.push_back(sum);
     return u;
 }
 
 std::vector<std::string> PID::GetColumnNames() {
     std::vector<std::string> column_names;
-    int ip_size = error.size();
-    for (int i{0}; i<ip_size; i++) {
+    for (int i{0}; i<num_states; i++) {
         column_names.push_back("U" + std::to_string(i));
     }
     return column_names;}
--- src/controllers/pid_controller.h
+++ src/controllers/pid_controller.h
@@ -12,27 +12,28 @@ class PID {
 
         void SetGains(const MatrixXd& KP, const MatrixXd& KI, const MatrixXd& KD);
 
-        void CalculateError(VectorXd x_ref, VectorXd x);
+        VectorXd GenerateControlInput(VectorXd x_err, VectorXd dxdt_err);
 
-        std::vector<VectorXd> GetErrorHistory();
+        std::vector<VectorXd> GetXErrorHistory();
+
+        std::vector<VectorXd> GetDxdtErrorHistory();
+
+        std::vector<VectorXd> GetIntegralErrorHistory();
 
         std::vector<VectorXd> GetControlInputHistory();
-
-        VectorXd GenerateControlInput();
 
         std::vector<std::string> GetColumnNames();
 
         friend std::ostream& operator<<(std::ostream& out, const PID& PID);
 
     private:
+        int num_states;
         MatrixXd kp;
         MatrixXd ki;
         MatrixXd kd;
-        VectorXd error;
-        VectorXd d_error;
-        VectorXd sum;
-        VectorXd previous_error;
-        std::vector<VectorXd> error_history;
+        std::vector<VectorXd> integral_err_history;
+        std::vector<VectorXd> x_err_history;
+        std::vector<VectorXd> dxdt_err_history;
         std::vector<VectorXd> control_input_history;
 
 };
